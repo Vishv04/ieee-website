32c7e2995b032316024d19280571d5beae49d36a
Vishv Boda <vishvboda0407@gmail.com>
2025-08-22 17:55:51 +0530

    Update authMiddleware.js

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -1,6 +1,9 @@
 const apiKey = process.env.API_KEY;
 
 const authMiddleware = async (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, x-api-key");
+    
     const apiKey = req.headers['x-api-key'];
     if (apiKey !== process.env.API_KEY) {
         return res.status(401).json({ error: "Unauthorized" });
